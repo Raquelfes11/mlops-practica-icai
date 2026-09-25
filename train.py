6a3cb75bae2c7a1d57e0bf4e479c2ee9ce3a16fe
Raquelfes11 <202209790@alu.comillas.edu>
2026-09-25 10:02:50 +0200

    Trust sklearn Tree type in log_model

--- train.py
+++ train.py
@@ -42,7 +42,11 @@ with mlflow.start_run():
     joblib.dump(model, 'model.pkl')
 
     # Registrar el modelo con MLflow
-    mlflow.sklearn.log_model(model, "random-forest-model")
+    mlflow.sklearn.log_model(
+        model,
+        name="random-forest-model",
+        skops_trusted_types=["sklearn.tree._tree.Tree"],
+    )
 
     # Registrar parámetros y métricas
     mlflow.log_param("n_estimators", 200)
